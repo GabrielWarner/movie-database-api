682a1aa5f34d796ad190552b186ac9f981b8e713
GabrielWarner <gabe0412@uw.edu>
2022-07-27 12:20:11 -0700

    built schema and seeds, also setup server

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO movies (movie_name)
+VALUES
+("I am Legend"),
+("Pursuit of Hapiness"),
+("Elf"),
+("Home Alone"),
+("Charlie and the Chocolate Factory");
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the movies_db database.`)
+);
+
+
+app.get('/api/movies' ,(req,res) =>{
+    db.query('SELECT * FROM movies',(err, results) => {
+        console.log(results)
+        res.json(results)
+    })
+})
+
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+ 
